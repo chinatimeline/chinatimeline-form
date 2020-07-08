dbda8eca3b788025ddf65f16b3e027da0fc989aa
duty-machine <63031384+duty-machine@users.noreply.github.com>
2020-07-07 23:00:11 -0300

    add submit

--- api/submit.js
+++ api/submit.js
@@ -1,4 +1,5 @@
 import { Octokit } from '@octokit/rest'
+import YAML from 'yaml'
 
 let TOKEN = process.env.TOKEN
 let REPOSITORY = process.env.REPOSITORY
@@ -6,18 +7,64 @@ let REPOSITORY = process.env.REPOSITORY
 let [owner, repo] = REPOSITORY.split('/')
 
 module.exports = async (req, res) => {
-  let { url } = req.body
+  let params = req.body
+
+  console.log(params)
+
+  let data = {
+    _id: 0,
+    topic: params['fields[topic]'],
+    date: params['fields[date]'],
+    title: params['fields[title]'],
+    significance: params['fields[significance]'],
+    event: params['fields[event]'],
+    url: params['fields[url]'],
+    type: params['fields[type]'],
+    image: params['fields[image]'],
+    caption: params['fields[caption]'],
+    submitTime: +new Date()
+  }
 
   let octokit = new Octokit({ auth: TOKEN })
-  let response = await octokit.issues.create({
+  let masterRef = await octokit.git.getRef({ owner, repo, ref: 'heads/master'})
+
+  let masterSha = masterRef.data.object.sha
+
+  let branchName = `tmp${+ new Date()}`
+  await octokit.git.createRef({ owner, repo, ref: `refs/heads/${branchName}`, sha: masterSha})
+
+  let ymlFilename = `${data.date}-entry${data.submitTime}.yml`
+
+  let buffer = new Buffer(YAML.stringify(data))
+  let base64 = buffer.toString('base64')
+
+  let committer = {
+    name: 'Your Name',
+    email: 'you@example.com'
+  }
+
+  await octokit.repos.createOrUpdateFileContents({
     owner,
     repo,
-    title: 'archive_request',
-    body: url
+    branch: branchName,
+    path: `_data/comments/${ymlFilename}`,
+    message: `add ${ymlFilename}`,
+    content: base64,
+    committer: committer,
+    author: committer
+  })
+
+  let createdPr = await octokit.pulls.create({
+    owner,
+    repo,
+    title: ymlFilename,
+    head: branchName,
+    base: 'master'
   })
 
   await new Promise(resolve => setTimeout(resolve, 1000))
 
-  res.setHeader('Location', response.data.html_url)
+  //res.setHeader('Location', `https://github.com/${REPOSITORY}/tree/${branchName}`)
+  res.setHeader('Location', createdPr.data.html_url)
   res.status(302).send('')
 }
