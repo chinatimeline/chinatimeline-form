ee569666287557b403fe59504b9fc4d7b4b3eaa8
duty-machine <63031384+duty-machine@users.noreply.github.com>
2020-07-07 22:02:22 -0300

    add dependencies

---
+++ api/submit.js
@@ -0,0 +1,23 @@
+import { Octokit } from '@octokit/rest'
+
+let TOKEN = process.env.TOKEN
+let REPOSITORY = process.env.REPOSITORY
+
+let [owner, repo] = REPOSITORY.split('/')
+
+module.exports = async (req, res) => {
+  let { url } = req.body
+
+  let octokit = new Octokit({ auth: TOKEN })
+  let response = await octokit.issues.create({
+    owner,
+    repo,
+    title: 'archive_request',
+    body: url
+  })
+
+  await new Promise(resolve => setTimeout(resolve, 1000))
+
+  res.setHeader('Location', response.data.html_url)
+  res.status(302).send('')
+}
