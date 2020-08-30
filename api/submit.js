df8d90a454fe62df71b57bb69f148b341e238272
chinatimeline <Your Email>
2020-08-30 03:51:35 +0000

    uuid

--- api/submit.js
+++ api/submit.js
@@ -1,6 +1,6 @@
 const { Octokit } = require("@octokit/rest");
 const YAML = require('yaml');
-const uuidv4 = require('uuidv4');
+const { v4: uuidv4 } = require('uuid');
 
 let TOKEN = process.env.TOKEN
 let REPOSITORY = process.env.REPOSITORY
