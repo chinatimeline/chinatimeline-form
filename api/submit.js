00f422908528dd5755441bc4282c27678ab90ed2
chinatimeline <Your Email>
2020-12-26 20:37:16 +0000

    fields musthave

--- api/submit.js
+++ api/submit.js
@@ -54,8 +54,8 @@ module.exports = async (req, res) => {
 
     await addFileToGit(path, content, prTitle)
 
-    await new Promise(resolve => setTimeout(resolve, 500))
   }
+  await new Promise(resolve => setTimeout(resolve, 500))
 
   res.setHeader('Location', `https://github.com/${REPOSITORY}/tree/${branchName}`)
 //   res.setHeader('Location', createdPr.data.html_url)
